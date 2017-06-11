3ab6cf549ef937a1737097ba2d1b20659d22c4a6
Silas Ferraciolli Corrêa <silascofc@gmail.com>
2017-06-11 10:56:13 -0300

    Alterando link da versao do gradle wrapper

---
+++ app/src/main/java/com/example/silas/caribbeanpirates/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.silas.caribbeanpirates;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
